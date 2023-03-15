a719bc262f3b093d66aa78b6a8ddebc97f7f49e1
Sümeyya Utku <89921699+kisacasumderler@users.noreply.github.com>
2023-03-15 23:39:42 +0300

    update

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ $(window).ready(function () {
   let bookBookmark = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M0 96C0 43 43 0 96 0h96V190.7c0 13.4 15.5 20.9 26 12.5L272 160l54 43.2c10.5 8.4 26 .9 26-12.5V0h32 32c17.7 0 32 14.3 32 32V352c0 17.7-14.3 32-32 32v64c17.7 0 32 14.3 32 32s-14.3 32-32 32H384 96c-53 0-96-43-96-96V96zM64 416c0 17.7 14.3 32 32 32H352V384H96c-17.7 0-32 14.3-32 32z"/></svg>`;
 
   let Card = (id, imghref, title, paragraph, webPageUrl, webPageIcon, githubRepoUrl, repoIcon) => {
-    return `<li id="${id}">
+      return `<li id="${id}">
     <div class="icerik">
         <div class="img">
             <img src="./img/${imghref}" alt="">
@@ -28,72 +28,92 @@ $(window).ready(function () {
   </li>`;
   }
   $.getJSON('./data/websiteler.json', function (data) {
-    let datas = data[2].data;
-    datas.sort(function (a, b) {
-      return b.id - a.id;
-    })
-    $.each(datas, function (key, value) {
-      let dataId = value.id;
-      let aciklama = value.aciklama;
-      let baslik = value.baslik;
-      let imgName = value.imgName;
-      let repUrl = value.repoUrl;
-      let siteUrl = value.siteUrl;
-      $('.Kapsam').append(Card(dataId, imgName, baslik, aciklama, siteUrl, arrowSvg, repUrl, bookBookmark));
-    });
+      let datas = data[2].data;
+      datas.sort(function (a, b) {
+          return b.id - a.id;
+      })
+      $.each(datas, function (key, value) {
+          let dataId = value.id;
+          let aciklama = value.aciklama;
+          let baslik = value.baslik;
+          let imgName = value.imgName;
+          let repUrl = value.repoUrl;
+          let siteUrl = value.siteUrl;
+          $('.Kapsam').append(Card(dataId, imgName, baslik, aciklama, siteUrl, arrowSvg, repUrl, bookBookmark));
+      });
 
-    let Kapsam = $(".Kapsam");
-    let li = $(".Kapsam li");
+      let Kapsam = $(".Kapsam");
+      let li = $(".Kapsam li");
 
-    let ToplamYuk = 0;
-    let KapsamYuk = Kapsam.outerHeight();
 
-    for (let i = 0; i < li.length; i++) {
-      ToplamYuk += li.eq(i).outerHeight(true);
-    }
+      let ToplamYuk = 0;
 
-    function Hesapla(t, k) {
-      if (t > k / 1.5) {
-        k += 10;
-        Kapsam.css("max-height", k + "px");
-        Hesapla(t, k);
+      let yukHesapla = (n) => {
+          for (let i = 0; i <= n; i++) {
+              ToplamYuk += li.eq(i).outerHeight(true);
+          }
+          return ToplamYuk;
       }
-    }
 
-    function HesaplaMobile(t, k) {
-      if (t > k / 2.5) {
-        k += 10;
-        Kapsam.css("max-height", k + "px");
-        Hesapla(t, k);
-      }
-    }
+      let ulMaxHeightBulD = () => {
+          let liMax = (li.length) - 1;
+          let li1 = Math.floor(liMax / 3);
+          let li2 = li1 + li1 + 1;
 
-    if($(window).outerWidth(true)>720) {
-      Hesapla(ToplamYuk, KapsamYuk);
-    }
-    if($(window).outerWidth(true)>=600 && $(window).outerWidth(true)<=720) {
-      HesaplaMobile(ToplamYuk, KapsamYuk);
-    }
-    if($(window).outerWidth(true)<600) {
-      Kapsam.removeAttr("style");
-      Kapsam.css("height","auto");
-    }
+          let li_1 = li.eq(li1) // 3/1 ye kadar 
+          let li3_2 = li.eq(li2); // 3/2 ye kadar 
+          let li3_3 = li.eq(liMax) // 3/3 e kadar max 
 
-    function checkWidth() {
-      if($(window).outerWidth(true)>720) {
-        Hesapla(ToplamYuk, KapsamYuk);
-      }
-      if($(window).outerWidth(true)>=600 && $(window).outerWidth(true)<=720) {
-        HesaplaMobile(ToplamYuk, KapsamYuk);
-      }
-      if($(window).outerWidth(true)<600) {
-        Kapsam.removeAttr("style");
-        Kapsam.css("height","auto");
-      }
-    }
+          let li1hesapla = yukHesapla(li1); // 0 - 8
+          let li2hesapla = yukHesapla(li2) - li1hesapla; // 0 -17 
+          let li3hesapla = yukHesapla(liMax) - li2hesapla; // 0 - 30
 
-    $(window).resize(checkWidth);
+
+          let maxUlHeight = Math.max(li1hesapla, li2hesapla, li3hesapla);
+          return maxUlHeight;
+      }
+
+      let ulMaxHeightBulM = () => {
+          let liMax = (li.length) - 1;
+          let li1 = Math.floor(liMax / 2);
+
+          let li2_2 = li.eq(liMax) // 2/2 ye kadar 
+          let li2_1 = li.eq(li1); // 2/1 e kadar 
+
+          let li1Hesapla = yukHesapla(li2_1);
+          let li2Hesapla = yukHesapla(li2_2) - li1Hesapla;
+
+          let maxUlHeight = Math.max(li1Hesapla, li2Hesapla);
+          return maxUlHeight;
+      }
+
+
+      let ulMaxHeightD = ulMaxHeightBulD();
+      let ulMaxHeightM = ulMaxHeightBulM();
+
+      let LoadPage = () => {
+          if ($(window).outerWidth(true) > 1200) {
+              Kapsam.css("height", ulMaxHeightD);
+              // desktop 
+          }
+          if ($(window).outerWidth(true) >= 720 && $(window).outerWidth(true) <= 1200) {
+              Kapsam.css("height", ulMaxHeightM);
+              // tablet
+          }
+          if ($(window).outerWidth(true) < 720) {
+              Kapsam.css("height", "auto");
+              // mobile 
+          }
+      }
+
+      LoadPage();
+
+      function checkWidth() {
+          LoadPage();
+      }
+
+      $(window).resize(checkWidth);
   });
   // 
-  
+
 })
